b286d84dbf251d9401e77162fd82e02c5cd989d1
the-dg04 <devgup04@gmail.com>
2024-09-29 21:02:43 +0530

    init

---
+++ src/main/java/com/echobeat/servingwebcontent/GreetingController2.java
@@ -0,0 +1,20 @@
+package com.echobeat.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController2 {
+
+	@GetMapping("/greetings")
+	public String gweeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("name2", name);
+		return "greeting2";
+	}
+
+}
+
+
