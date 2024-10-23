c8b7619fa6c78170d8fc1bbf6c5528128f0edfba
fireheart17 <iampiyushgoyal17@gmail.com>
2024-10-24 04:49:19 +0530

    added current user functionality

--- src/main/java/com/echobeat/servingwebcontent/controller/fyp.java
+++ src/main/java/com/echobeat/servingwebcontent/controller/fyp.java
@@ -50,39 +50,6 @@ public class fyp {
 
         model.addAttribute("topPicks", topPicks);
         model.addAttribute("newTracks", newTracks);
-        // System.out.println("response : "+Arrays.toString(response));
-
-        // Map<String, String> response = webClientBuilder.build()
-        //         .get()
-        //         .uri("https://jsonplaceholder.typicode.com/posts")
-        //         .retrieve()
-        //         .bodyToMono(new ParameterizedTypeReference<Map<String, String>[]>() {})
-        //         .block();
-
-        // System.out.println("response : "+response);
-
-        // POST REQUEST
-        // webClientBuilder.build()
-        //         .post()
-        //         .uri(url)
-        //         .header(HttpHeaders.AUTHORIZATION, "Bearer " + authToken) // Set the Authorization header
-        //         .contentType(MediaType.APPLICATION_JSON) // Set the Content-Type to JSON
-        //         .bodyValue(requestBody) // Set the body
-        //         .retrieve()
-        //         .bodyToMono(Map.class); 
-
-        // songname.add("song1");
-        // songname.add("song2");
-        // songname.add("song3");
-        // songname.add("song4");
-        // songname.add("song5");
-        // songname.add("song6");
-        // songname.add("song7");
-        // songname.add("song8");
-        // songname.add("song9");
-        // songname.add("song10");
-        // // songname.add(response.get("track_name"));
-        // model.addAttribute("songs", songname);
         return "fyp";
     }
 }
--- src/main/java/com/echobeat/servingwebcontent/controller/user.java
+++ src/main/java/com/echobeat/servingwebcontent/controller/user.java
@@ -1,10 +1,14 @@
 package com.echobeat.servingwebcontent.controller;
 
-import java.time.LocalTime;  
+import java.time.LocalTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
 
 class User {
     public long userId;
@@ -31,10 +35,22 @@ class User {
 
 @Controller
 public class user {
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/user")
     public String getUser(@RequestParam(name="id", required=false, defaultValue="0") String name, Model model) {
-        int id = Integer.parseInt(name);
-        User user=new User(id, "Abhigyan", "Pandey", "password", LocalTime.now(), 21, "Male", 100, "23/10/2024");
+        // int id = Integer.parseInt(name);
+        
+        User user = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8081/api/user/"+name)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<User>() {})
+                .block();
+
+        // User user=new User(id, "Abhigyan", "Pandey", "password", LocalTime.now(), 21, "Male", 100, "23/10/2024");
         model.addAttribute("user", user);
         return "user";
     }
