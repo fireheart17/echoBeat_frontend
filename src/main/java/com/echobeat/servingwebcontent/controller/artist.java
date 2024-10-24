0138a447c3155913e133885f76632195aa16f017
fireheart17 <iampiyushgoyal17@gmail.com>
2024-10-24 15:57:54 +0530

    connected artist and user to backend

--- src/main/java/com/echobeat/servingwebcontent/controller/artist.java
+++ src/main/java/com/echobeat/servingwebcontent/controller/artist.java
@@ -2,57 +2,71 @@ package com.echobeat.servingwebcontent.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
 
-class Song {
 
-    public String name;
-    public int id;
-
-    public Song(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-}
-
-class Album {
-
-    public String name;
-    public int id;
-
-    public Album(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-}
+import java.util.*;
 
 @Controller
 public class artist {
 
-    @GetMapping("/artist")
-    public String getArtist(Model model) {
-        List<Song> songs = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            String s = "song";
-            char id = (char) ('0' + i);
-            s = s + id;
-            Song newSong = new Song(i, s);
-            songs.add(newSong);
-        }
-        List<Album> albums = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            String s = "album";
-            char id = (char) ('0' + i);
-            s = s + id;
-            Album newAlbum = new Album(i, s);
-            albums.add(newAlbum);
-        }
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
-        model.addAttribute("songs", songs);
+    // @GetMapping("/user")
+    // public String getUser(@RequestParam(name="id", required=false, defaultValue="0") String name, Model model) {
+    //     // int id = Integer.parseInt(name);
+        
+    //     User user = webClientBuilder.build()
+    //             .get()
+    //             .uri("http://localhost:8081/api/user/"+name)
+    //             .retrieve()
+    //             .bodyToMono(new ParameterizedTypeReference<User>() {})
+    //             .block();
+
+    //     // User user=new User(id, "Abhigyan", "Pandey", "password", LocalTime.now(), 21, "Male", 100, "23/10/2024");
+    //     model.addAttribute("user", user);
+    //     return "user";
+    // }
+
+    @GetMapping("/artist")
+    public String getArtist(@RequestParam(name="id", required=false, defaultValue="1") String name, Model model) {
+
+        Map<String,String> artist = webClientBuilder.build()
+            .get()
+            .uri("http://localhost:8081/api/artists/"+name)
+            .retrieve()
+            .bodyToMono(new ParameterizedTypeReference<Map<String,String>>() {})
+            .block();
+        
+        Map<String,String>[] tracks = webClientBuilder.build()
+            .get()
+            .uri("http://localhost:8081/api/artisttracks/"+name)
+            .retrieve()
+            .bodyToMono(new ParameterizedTypeReference<Map<String,String>[]>() {})
+            .block();
+        
+        System.out.println(Arrays.toString(tracks));
+
+        Map<String,String>[] albums = webClientBuilder.build()
+            .get()
+            .uri("http://localhost:8081/api/artistalbums/"+name)
+            .retrieve()
+            .bodyToMono(new ParameterizedTypeReference<Map<String,String>[]>() {})
+            .block();
+
+        System.out.println(Arrays.toString(albums));
+        
+        model.addAttribute("artist", artist);
+        model.addAttribute("tracks", tracks);
         model.addAttribute("albums", albums);
         return "artist";
     }
--- src/main/java/com/echobeat/servingwebcontent/controller/user.java
+++ src/main/java/com/echobeat/servingwebcontent/controller/user.java
@@ -10,28 +10,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 
-class User {
-    public long userId;
-    public String firstName;
-    public String lastName;
-    public String password;
-    public LocalTime dob;
-    public int age;
-    public String gender;
-    public long  subscriptionId;
-    public String subscriptionEndDate;
+import java.util.*;
 
-    User(long userId, String firstName, String lastName, String password, LocalTime dob, int age, String geder, long subscriptionId, String subscriptionEndDate){
-        this.userId=userId;
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.password=password;
-        this.dob=dob;   
-        this.age=age;
-        this.subscriptionEndDate=subscriptionEndDate;
-        this.subscriptionId=subscriptionId;
-    }
-}
+// class User {
+//     public long userId;
+//     public String firstName;
+//     public String lastName;
+//     public String password;
+//     public LocalTime dob;
+//     public int age;
+//     public String gender;
+//     public long  subscriptionId;
+//     public String subscriptionEndDate;
+
+//     User(long userId, String firstName, String lastName, String password, LocalTime dob, int age, String geder, long subscriptionId, String subscriptionEndDate){
+//         this.userId=userId;
+//         this.firstName=firstName;
+//         this.lastName=lastName;
+//         this.password=password;
+//         this.dob=dob;   
+//         this.age=age;
+//         this.subscriptionEndDate=subscriptionEndDate;
+//         this.subscriptionId=subscriptionId;
+//     }
+// }
 
 @Controller
 public class user {
@@ -40,14 +42,14 @@ public class user {
     private WebClient.Builder webClientBuilder;
 
     @GetMapping("/user")
-    public String getUser(@RequestParam(name="id", required=false, defaultValue="0") String name, Model model) {
+    public String getUser(@RequestParam(name="id", required=false, defaultValue="1") String name, Model model) {
         // int id = Integer.parseInt(name);
         
-        User user = webClientBuilder.build()
+        Map<String,String> user = webClientBuilder.build()
                 .get()
                 .uri("http://localhost:8081/api/user/"+name)
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<User>() {})
+                .bodyToMono(new ParameterizedTypeReference<Map<String,String>>() {})
                 .block();
 
         // User user=new User(id, "Abhigyan", "Pandey", "password", LocalTime.now(), 21, "Male", 100, "23/10/2024");
